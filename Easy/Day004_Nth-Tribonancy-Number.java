b6fa9e01299a23db3579eb454f3022ce924ed33f
Nilesh Kadam <nileshkadam1118@gmail.com>
2026-04-24 22:43:17 +0530

    Fix comment formatting in Tribonacci number explanation

--- Easy/Day004_Nth-Tribonancy-Number.java
+++ Easy/Day004_Nth-Tribonancy-Number.java
@@ -5,7 +5,7 @@ public class TribonacciSolutions {
     // --------------------------------------------------
     /*       
      * Idea:          
-     * Tribonacci is similar to Fibonacci, but instead of 2,
+     * Tribonacci is similar to Fibonacci, but instead of 2,         
      * we sum the last 3 numbers.
      * 
      * T(n) = T(n-1) + T(n-2) + T(n-3)
