be10044c4707ae7b74b8ea4b0426dc9df050a201
Nilesh Kadam <nileshkadam1118@gmail.com>
2026-04-27 22:38:15 +0530

    Update problem statement and approach in comments

--- Easy/Day005_WaterBottles.java
+++ Easy/Day005_WaterBottles.java
@@ -1,6 +1,6 @@
 /**      
- * Problem: 1518. Water Bottles          
- * * Approach: Simulation
+ * Problem: 1518. Water Bottles                 
+ * * Approach: Simulation     
  * We simulate the process of drinking bottles and exchanging the empty ones.
  * This is a greedy approach because we exchange as many bottles as possible 
  * at every available opportunity.
